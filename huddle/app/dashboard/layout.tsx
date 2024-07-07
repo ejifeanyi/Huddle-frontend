b9de48bea649023a62e668aa6e04cbbe6bee1a9d
the2minengineer <ifeanyiemmanueljoseph@gmail.com>
2024-07-07 04:55:49 +0100

    second commit - added dashboard and layout refactoring

---
+++ huddle/app/dashboard/layout.tsx
@@ -0,0 +1,15 @@
+import Sidebar from "@/components/Sidebar";
+
+export default function DashboardLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<section className="flex">
+			<Sidebar />
+
+			{children}
+		</section>
+	);
+}
--- huddle/app/page.tsx
+++ huddle/app/page.tsx
@@ -1,62 +1,7 @@
-"use client";
+import React from "react";
 
-import { Button } from "@nextui-org/button";
-import { LogOut } from "lucide-react";
-import { useState } from "react";
-
-interface Project {
-	id: number;
-	name: string;
-}
-
-const Page: React.FC = () => {
-	const [projects, setProjects] = useState<Project[]>([]);
-	const [nextProjectId, setNextProjectId] = useState<number>(1);
-
-	const addProject = () => {
-		const newProjectName = prompt("Enter project name:");
-		if (newProjectName) {
-			const newProject: Project = { id: nextProjectId, name: newProjectName };
-			setProjects([...projects, newProject]);
-			setNextProjectId(nextProjectId + 1);
-		}
-	};
-
-	return (
-		<div className="flex">
-			{/* side bar */}
-			<div className="pt-8 pb-20 shadow-xl w-[260px] min-h-screen flex flex-col justify-between">
-				{/* logo and projects */}
-				<div>
-					<p className="font-bold ml-10 mb-14">Huddle</p>
-					<Button
-						color="primary"
-						onClick={addProject}
-						className="ml-10 mb-10"
-					>
-						Create New Project
-					</Button>
-					{projects.map((project) => (
-						<p
-							key={project.id}
-							className="font-medium hover:bg-[#973FCF]/10 text-[#707070] hover:text-black py-[10px] bg-white pl-10 mb-1 cursor-pointer"
-						>
-							{project.name}
-						</p>
-					))}
-				</div>
-
-				{/* logout button */}
-				<p className="flex items-center gap-2 font-medium hover:bg-[#973FCF]/10 text-[#707070] hover:text-black py-[10px] bg-white pl-10 cursor-pointer">
-					<span>{<LogOut />}</span>
-					Log out
-				</p>
-			</div>
-
-			{/* dashboard */}
-			<h1>Dashboard</h1>
-		</div>
-	);
+const page = () => {
+	return <div>Landing page!!!!</div>;
 };
 
-export default Page;
+export default page;
